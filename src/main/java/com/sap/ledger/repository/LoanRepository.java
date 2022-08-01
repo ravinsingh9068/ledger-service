aa78f015b31ca650bc6408f9aec461a6b0508d1d
ravindersingh-psag <91446067+ravindersingh-psag@users.noreply.github.com>
2022-08-02 03:50:01 +0530

    first commit

---
+++ src/main/java/com/sap/ledger/repository/LoanRepository.java
@@ -0,0 +1,11 @@
+package com.sap.ledger.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sap.ledger.entity.Loan;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+}
